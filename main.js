b08c4a7d2da8520a48ffdc55362a8347771c9b65
UT-99 <utkarshtiwari922@gmail.com>
2022-11-28 19:32:13 +0530

    First Commit Digital Clock JS

---
+++ main.js
@@ -0,0 +1,8 @@
+function time(){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    console.log(hours  + " " +  minutes +  " " +  seconds)
+}
