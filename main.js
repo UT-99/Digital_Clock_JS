305351df5927fe0b1ef2319a9cf05a2979efb63f
UT-99 <utkarshtiwari922@gmail.com>
2022-12-03 15:04:57 +0530

    Time Added to the clock in HTML page and Time details are also fetched on Set Alarm button

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
 function time(){
     let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let hrs = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let AMPM = '' ;
 
-    console.log(hours  + " " +  minutes +  " " +  seconds)
+
+    
+    
+    document.getElementById('mins').innerText = min + '\n'+ ' minutes';
+    document.getElementById('sec').innerText = sec + '\n'+' seconds';
+    
+
+    if (hrs > 12 ){
+        hrs =  hrs - 12  ;
+        AMPM = 'PM';
+        document.getElementById('hours').innerText = hrs +   '\n'+ ' hours';
+        document.getElementById('AMPM').innerText = AMPM;
+
+    }
+    else {
+        document.getElementById('hours').innerText = hrs +   '\n'+ ' hours';
+        AMPM = 'AM';
+        document.getElementById('AMPM').innerText = AMPM;
+    }
+ 
+
+    
 }
 
+time();
+setInterval( time , 1000)
+
+
+function getTimeDetails(){
+    var takeWakeUpValue = document.getElementById('wakeUpTimeId').value;
+    var LunchTimeValue = document.getElementById('LunchTimeId').value;
+    var NightTimeValue = document.getElementById('NightTimeId').value;
+    var SetNapTimeValue = document.getElementById('SetNapTimeId').value;
+    
+    document.getElementById('Wake').innerText ="Wake Up Time is : " + takeWakeUpValue;
+    document.getElementById('lunch').innerText ="Lunch Time is   : " + LunchTimeValue;
+    document.getElementById('nap').innerText ="Nap Time is     : " + NightTimeValue;
+    document.getElementById('night').innerText ="Night Time is   : " + SetNapTimeValue;
+}
 
