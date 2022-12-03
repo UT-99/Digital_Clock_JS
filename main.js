a7b89f56960dd4a3eccfdc0ad3d893a269809191
UT-99 <utkarshtiwari922@gmail.com>
2022-12-03 17:41:08 +0530

    Image changes are added as per time

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 function time(){
-    let date = new Date();
-    let hrs = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    let AMPM = '' ;
+    var date = new Date();
+    var hrs = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var AMPM = '' ;
 
 
     
     
-    document.getElementById('mins').innerText = min + '\n'+ ' minutes';
-    document.getElementById('sec').innerText = sec + '\n'+' seconds';
+    document.getElementById('mins').innerText = min + '\n'+ ' minute';
+    document.getElementById('sec').innerText = sec + '\n'+' second';
     
 
     if (hrs > 12 ){
         hrs =  hrs - 12  ;
         AMPM = 'PM';
-        document.getElementById('hours').innerText = hrs +   '\n'+ ' hours';
+        document.getElementById('hours').innerText = hrs +   '\n'+ ' hour';
         document.getElementById('AMPM').innerText = AMPM;
 
     }
     else {
-        document.getElementById('hours').innerText = hrs +   '\n'+ ' hours';
+        document.getElementById('hours').innerText = hrs +   '\n'+ ' hour';
         AMPM = 'AM';
         document.getElementById('AMPM').innerText = AMPM;
     }
@@ -33,15 +33,149 @@ time();
 setInterval( time , 1000)
 
 
+// For image Changing Code 
+
 function getTimeDetails(){
+
+
+    // For time Calculation 
+    var date = new Date();
+    var hrs = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var AMPM = '' ;   
+    
+    document.getElementById('mins').innerText = min + '\n'+ ' minute';
+    document.getElementById('sec').innerText = sec + '\n'+' second';
+    
+  
+
+    if ( hrs > 12 ){
+        hrs =  hrs - 12  ;
+        AMPM = 'PM';
+        document.getElementById('hours').innerText = hrs +   '\n'+ ' hour';
+        document.getElementById('AMPM').innerText = AMPM;
+
+    }
+    else {
+        document.getElementById('hours').innerText = hrs +   '\n'+ ' hour';
+        AMPM = 'AM';
+        document.getElementById('AMPM').innerText = AMPM;
+    }
+ 
+
     var takeWakeUpValue = document.getElementById('wakeUpTimeId').value;
     var LunchTimeValue = document.getElementById('LunchTimeId').value;
     var NightTimeValue = document.getElementById('NightTimeId').value;
     var SetNapTimeValue = document.getElementById('SetNapTimeId').value;
     
+    // Setting value on Page 
+
     document.getElementById('Wake').innerText ="Wake Up Time is : " + takeWakeUpValue;
     document.getElementById('lunch').innerText ="Lunch Time is   : " + LunchTimeValue;
-    document.getElementById('nap').innerText ="Nap Time is     : " + NightTimeValue;
-    document.getElementById('night').innerText ="Night Time is   : " + SetNapTimeValue;
+    document.getElementById('nap').innerText ="Nap Time is     : " +  SetNapTimeValue;
+    document.getElementById('night').innerText ="Night Time is   : " +NightTimeValue ;
+
+    var arrWakeTime = takeWakeUpValue.toString().split(" ");
+    var arrLunchTime = LunchTimeValue.toString().split(" ");
+    var arrNapTime = SetNapTimeValue.toString().split(" ");
+    var arrNightTime = NightTimeValue.toString().split(" ");
+
+
+    console.log('Wake Up Time : ' ,   arrWakeTime);
+    console.log('Lunch Time  ', arrLunchTime);
+    console.log('Night Time   ', arrNightTime);
+    console.log('Nap Time is  ', arrNapTime);
+    
+    var img = '';
+
+
+    if ( (arrWakeTime[0] === arrLunchTime[0]  === arrNapTime[0] ===  arrNightTime [0]) === (hrs+AMPM) ){
+        console.log("Yes all the times are equal so displaying the welcome home image  ");
+        img = "<img  src='assets/welcomeHome.jpg' alt=''></img>"
+    }
+
+   else if (arrWakeTime[0] === (hrs+AMPM)){
+            img = "<img  src='assets/Component 30 – 1.svg' alt=''>"
+            document.getElementById('imageTag').innerHTML  = img;
+           document.getElementById('imageTag').style.height ='70%'
+           document.getElementById('imageTag').style.width ='70%'
+           
+            
+            console.log("Yes they  is our wake up time  ")
+
+
+   }
+
+   else if (arrLunchTime[0] === (hrs+AMPM)){
+        img = "<img  src='assets/Component 31 – 1.svg' alt=''>"
+        document.getElementById('imageTag').innerHTML  = img;
+        document.getElementById('imageTag').style.height ='100%'
+        document.getElementById('imageTag').style.width ='70%'
+   
+    
+        console.log("Yes they  is our lunch time  ")
+   }
+
+
+   else if (arrNapTime[0] === (hrs+AMPM)){
+    img = "<img  src='assets/goodEveningImage.png' alt=''>"
+    document.getElementById('imageTag').innerHTML  = img;
+    document.getElementById('imageTag').style.height ='100%'
+    document.getElementById('imageTag').style.width ='70%'
+
+
+    console.log("Yes they  is our evening  time  ")
+    }
+
+
+    else if (arrNightTime[0] === (hrs+AMPM))
+    {
+        img = "<img  src='assets/goodNightImage.png' alt=''>"
+        document.getElementById('imageTag').innerHTML  = img;
+        document.getElementById('imageTag').style.height ='100%'
+        document.getElementById('imageTag').style.width ='70%'
+
+
+        console.log("Yes they  is our night time  ")
+    }
+
+
+/*
+    else {
+        if ( arrNapTime[0] === (hrs+AMPM)){
+            img = "<img  src='assets/goodNightImage.png' alt=''>"
+            document.getElementById('imageTag').innerHTML  = img;
+           document.getElementById('imageTag').style.height ='70%'
+           document.getElementById('imageTag').style.width ='70%'
+           
+            
+            console.log("Yes they are equal ")
+        }
+    }
+
+    */
+ 
+
+ /*
+    var img = " <img  src='assets/goodNightImage.png' alt=''>" 
+    if ( arrNapTime[0] === (hrs+AMPM)){
+        document.getElementById('imageTag').innerHTML  = img;
+       document.getElementById('imageTag').style.height ='70%'
+       document.getElementById('imageTag').style.width ='70%'
+        
+        console.log("Yes they are equal ")
+    }
+
+*/
+    // Testing purpose 
+    
+    
+    console.log(hrs);
+    console.log(AMPM);
+
+
+
+    
 }
 
